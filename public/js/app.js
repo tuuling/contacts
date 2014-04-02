707ec3e629fca73b58da4feb910f733506a9cb49
Reedik Tuuling <reedik.tuuling@gmail.com>
2014-04-02 21:36:22 +0300

    list repeater

--- public/js/app.js
+++ public/js/app.js
@@ -3,8 +3,28 @@
 
   app = angular.module('contactList', []);
 
-  app.controller('appCtrl', function($scope) {
-    return $scope.name = 'Testname';
+  app.controller('ContactListCtrl', function($scope) {
+    $scope.contactList = [
+      {
+        firstname: 'Reedik',
+        lastname: 'Tuuling',
+        phone: '53241',
+        group: 'work'
+      }, {
+        firstname: 'Kaisa',
+        lastname: 'Tuuling',
+        phone: '53241',
+        group: 'home'
+      }, {
+        firstname: 'Oliver',
+        lastname: 'Tuuling',
+        phone: '53241',
+        group: 'work'
+      }
+    ];
+    return $scope.addContant = function() {
+      return console.log('addcontanct');
+    };
   });
 
 }).call(this);
--- src/app.coffee
+++ src/app.coffee
@@ -1,4 +1,23 @@
 app = angular.module 'contactList', []
 
-app.controller 'appCtrl', ($scope) ->
-  $scope.name = 'Testname'
+app.controller 'ContactListCtrl', ($scope) ->
+
+  $scope.contactList = [
+    firstname: 'Reedik'
+    lastname: 'Tuuling'
+    phone: '53241'
+    group: 'work'
+  ,
+    firstname: 'Kaisa'
+    lastname: 'Tuuling'
+    phone: '53241'
+    group: 'home'
+  ,
+    firstname: 'Oliver'
+    lastname: 'Tuuling'
+    phone: '53241'
+    group: 'work'
+  ]
+
+  $scope.addContant = () ->
+    console.log('addcontanct')
